b6edc6b120395214d107ed8ac770e64d6ce90480
ibrahimali123 <ibrahim.ali.0403@gmail.com>
2015-12-02 10:43:47 +0200

    Fixing Errors

--- gen/com/findmydevice/R.java
+++ gen/com/findmydevice/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package com.findmydevise;
+package com.findmydevice;
 
 public final class R {
     public static final class attr {
--- src/com/findmydevice/AddDeviceActivity.java
+++ src/com/findmydevice/AddDeviceActivity.java
@@ -1,7 +1,5 @@
 package com.findmydevice;
 
-import com.findmydevise.R;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
